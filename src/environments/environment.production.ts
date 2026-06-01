e0aa54b832288f2d74b86570ebe76de29f1cf217
James Guerra <james.guerra2008@gmail.com>
2026-06-01 15:50:51 +1000

    refactor avatar styles

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  supabase: {
+    url: 'https://dcsvugfsaawxmlqumimi.supabase.co',
+    anonKey: 'sb_publishable_hCKnHRoTYfEGptTRnwhB8w_RmyX-nUD',
+  },
+  apiUrl: 'https://team-flow-api-i18b.onrender.com',
+  workspaceId: '019e6d17-59e9-7a68-98e5-6d203abbbc95',
+};
